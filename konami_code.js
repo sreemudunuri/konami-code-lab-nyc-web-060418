db31b2f577c525ad5ad7412ca7689e56a6cb15be
Sree <harsha6raju@yahoo.com>
2018-07-19 17:37:04 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
-}
+  let idx = 0
+  document.body.addEventListener('keydown', function(event){
+    const key = event.key
+    if (key === codes[idx]) {
+      console.log(event.key)
+      console.log(idx)
+      idx ++
+
+      if(idx === codes.length) {
+        alert('You Won a puppy!')
+        idx = 0
+      }
+
+    } else {
+        idx = 0
+    }
+  })
+};
